028be69973680e610dea8a7d856f34e21c277424
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-07-17 07:42:54 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
-class Dog
+# class Dog
+#     def name=(dog_name)
+#         this_dogs_name = dog_name
+#     end
+    
+#     def name
+#         this_dogs_name
+#     end
+# end
 
-end
+class Dog
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+
+lassie = Dog.new
+
+lassie.name = "lassie"
+
+puts lassie.name
+
+
+# With ruby OOP, to set attributes to a particular class, you need two #methods. (#getter and #setter)
+# The setter method has an argument and we set the instance variable to the value (argument)
